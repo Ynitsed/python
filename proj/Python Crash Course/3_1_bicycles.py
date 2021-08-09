3791f10116484948b9a604efe2b73d5d669c4e2e
Ynitsed <gabriel.lu619@gmail.com>
2021-08-08 23:11:52 -0400

    updated

---
+++ proj/Python Crash Course/3_1_bicycles.py
@@ -0,0 +1,8 @@
+# Page 38
+bicycles = ['trek', 'cannondale', 'redline', 'spacialized']
+print(bicycles)
+
+print(bicycles[0].title())
+print(bicycles[1].title())
+print(bicycles[3].title())
+print(bicycles[-1].title())
---
+++ proj/Python Crash Course/3_2_motorcycles.py
@@ -0,0 +1,16 @@
+# Page 41
+motorcycles = ['honda', 'yamaha', 'suzuku']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('bmw')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
