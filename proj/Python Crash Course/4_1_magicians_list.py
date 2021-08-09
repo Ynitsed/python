a43df3f57e5b6480398cb01414d44d041f83ff34
Gabriel <gabriel.lu619@gmail.com>
2021-08-09 00:30:57 -0400

    updated

--- proj/Python Crash Course/4_1_magicians_list.py
+++ proj/Python Crash Course/4_1_magicians_list.py
@@ -1,23 +1,21 @@
 # Page 54
+print("\n------------------------------------------\n")
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
         print(magician.title() + ", that was a great trick!\n")
         print("I can't wait to see your next trick, " + magician.title() + ".\n")
-
 print("\n------------------------------------------\n")
 #   for cat in cats:
 #   for dog in dogs:
 #   for item in list_of_items:
-
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
        print(magician.title() + ", that was a great trick!")
        print("I can't wait to see your next trick, " + magician.title() + ".\n")
 
        print("Thank you everyone, that was a great magic show!")
-
 print("\n------------------------------------------\n")
-
 magicians = ['alice', 'david', 'carolina'] 
 for magician in magicians:
        print(magician)
+print("\n------------------------------------------\n")
