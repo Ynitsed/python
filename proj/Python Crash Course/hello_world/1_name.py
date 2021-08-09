018727755f79ed44f8654c0b6324df283e1a95ae
Ynitsed <gabriel.lu619@gmail.com>
2021-08-08 22:09:13 -0400

    updated

---
+++ proj/Python Crash Course/hello_world/1_name.py
@@ -0,0 +1,12 @@
+# Chapter 2 Page 24 1_name.py
+name = "gabriel k. lu\n"
+print(name.title())
+
+#In this example, the lowercase string "ada lovelace" is stored in the variable
+#name . The method title() appears after the variable in the print() statement. 
+#A method is an action that Python can perform on a piece of data. The
+#dot ( . ) after name in name.title() tells Python to make the title() method
+#act on the variable name .
+
+print(name.upper())
+print(name.lower())
--- proj/Python Crash Course/hello_world/hello_world.py
+++
@@ -1,4 +0,0 @@
-message1 = "Hello Python world!"
-message2 = "Hello Python Crash Course world!"
-
-print(message1 + "\n" + message2)
